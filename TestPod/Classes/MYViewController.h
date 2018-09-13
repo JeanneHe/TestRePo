8d2a08e22259e04b0164808565a24bcd5e7a6667
hexiaoguang <hexiaoguang@91sph.com>
2018-09-13 16:14:19 +0800

    de bug

---
+++ TestPod/Classes/MYViewController.h
@@ -0,0 +1,13 @@
+//
+//  MYViewController.h
+//  test
+//
+//  Created by 何晓光 on 2018/9/13.
+//  Copyright © 2018年 HXG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MYViewController : UIViewController
+
+@end
---
+++ TestPod/Classes/MYViewController.m
@@ -0,0 +1,38 @@
+//
+//  MYViewController.m
+//  test
+//
+//  Created by 何晓光 on 2018/9/13.
+//  Copyright © 2018年 HXG. All rights reserved.
+//
+
+#import "MYViewController.h"
+
+@interface MYViewController ()
+
+@end
+
+@implementation MYViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self.view setBackgroundColor:[UIColor greenColor]];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
