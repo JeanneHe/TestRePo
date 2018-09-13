79ea7484e67c2cee8b7952c9b5dccd5d62910a2f
hexiaoguang <hexiaoguang@91sph.com>
2018-09-13 16:31:34 +0800

    debug

--- TestPod/Classes/ViewController.m
+++ TestPod/Classes/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import "UIImageView+WebCache.h"
+#import "MYViewController.h"
 
 @interface ViewController ()
 
@@ -18,14 +19,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    UIImageView *temp = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
-    [temp setBackgroundColor:[UIColor blackColor]];
-    [temp sd_setImageWithURL:[NSURL URLWithString:@"http://icon.58pic.com/wangEditImg/2018/09/06/17/64bcdea8929bddc3303a0cb1ac394eee.jpg"]];
-    //    http://img.91sph.com/goods//20180830//006c3e6c4c2944fc8b81d4eb3920077d_s1.jpg
-    [self.view addSubview:temp];
-    temp = nil;
+//    UIImageView *temp = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 200, 200)];
+//    [temp setBackgroundColor:[UIColor blackColor]];
+//    [temp sd_setImageWithURL:[NSURL URLWithString:@"http://icon.58pic.com/wangEditImg/2018/09/06/17/64bcdea8929bddc3303a0cb1ac394eee.jpg"]];
+//    //    http://img.91sph.com/goods//20180830//006c3e6c4c2944fc8b81d4eb3920077d_s1.jpg
+//    [self.view addSubview:temp];
+//    temp = nil;
+    
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    MYViewController *temp = [MYViewController new];
+    [self presentViewController:temp animated:YES completion:^{
+    }];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
